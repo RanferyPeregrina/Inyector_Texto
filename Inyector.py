2ac90722b7f0e0694267ca59be78b4d8c0cbc877
Ranfery <88330135+RanferyPeregrina@users.noreply.github.com>
2026-06-25 01:29:29 -0600

    Avances Inyector de texto
    Ya se puede enlistar y seleccionar los archivos de texto dentro de la carpeta.

--- Inyector.py
+++ Inyector.py
@@ -1,9 +1,23 @@
 import os
 
+
+CarpetaTextos = 'Textos'
+
 print("\n" * 5)
 print("=" * 30)
 print("Programa que inyecta texto de una fuente en un formato específico.")
 print("Selecciona tu archivo: ")
-Directorios = os.listdir()
-for archivo_paraLeer in Directorios:
-    print(archivo_paraLeer)
+Directorios = os.listdir(CarpetaTextos)
+for Indice, archivo_paraLeer in enumerate(Directorios):
+    print(f"{Indice}.- {archivo_paraLeer}")
+print(f"{Indice + 1}.- 'Otros'")
+ArchivoElegido = int(input("Ingrese el numero de archivo que quiere leer:  "))
+
+if ArchivoElegido.lower() == "otros":
+    pass #Aquí lo que va a pasar es una interfaz que abra una ventana de Windows para elegir tu archivo
+else:
+    ArchivoElegido = os.path.join(CarpetaTextos, Directorios[ArchivoElegido])
+    with open(ArchivoElegido, "r", encoding="utf-8") as Archivo:
+        TextoLeido = Archivo.read()
+        print("El contenido leído es el siguiente: ")
+        print(TextoLeido)
